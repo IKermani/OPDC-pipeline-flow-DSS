ee6fd10bae28bc96b9171b316fb1306cdeb3ad82
fischer <imankermani@hotmail.com>
2021-08-09 20:27:12 +0430

    configuring heroku files

--- DSS/settings.py
+++ DSS/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+# Configure Django App for Heroku.
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
